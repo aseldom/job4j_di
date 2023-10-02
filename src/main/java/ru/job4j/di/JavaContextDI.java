9847944f958c6fa7d5f0dfbfdd6c3ccf8b4549c5
aseldom <a.seldom@gmail.com>
2023-10-02 16:29:23 +0600

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505070 #449500]

---
+++ src/main/java/ru/job4j/di/JavaContextDI.java
@@ -0,0 +1,14 @@
+package ru.job4j.di;
+
+public class JavaContextDI {
+    public static void main(String[] args) {
+        /*
+        var context = new Context();
+        context.searchInPackage("ru.job4j");
+        var ui = context.get(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
+         */
+    }
+}
