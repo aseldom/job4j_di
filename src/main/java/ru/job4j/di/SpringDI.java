8dcdb3756ca77480a356b0945897d7b43d486783
aseldom <a.seldom@gmail.com>
2024-01-23 12:28:22 +0600

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -8,12 +8,10 @@ public class SpringDI {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.scan("ru.job4j.di");
         context.refresh();
-        StartUI ui = context.getBean(StartUI.class);
-        ui.add("Petr Arsentev");
-        ui.add("Ivan ivanov");
-        ui.print();
+        Store store = context.getBean(Store.class);
+        store.add("Petr Arsentev");
+        Store another = context.getBean(Store.class);
+        another.getAll().forEach(System.out::println);
 
-        String answer = ui.getConsoleInput().askStr("Enter anything: ");
-        System.out.printf("Answer is: %s", answer);
     }
 }
